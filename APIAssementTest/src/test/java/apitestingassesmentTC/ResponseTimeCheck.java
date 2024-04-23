2009459ea73b40f6f1f95ad7dd347e6d21e4ee18
surajdatkhile <58423506+surajdatkhile@users.noreply.github.com>
2024-04-23 18:21:59 +0530

    Update ResponseTimeCheck.java

--- APIAssementTest/src/test/java/apitestingassesmentTC/ResponseTimeCheck.java
+++ APIAssementTest/src/test/java/apitestingassesmentTC/ResponseTimeCheck.java
@@ -1,37 +1,34 @@
-package apitestingassesmentTC;
+package apiTestingTC;
 import java.util.concurrent.TimeUnit;
+
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.response.Response.*;
 import static org.hamcrest.Matchers.*;
-public class ResponseTimeCheck {
+public class ApiResponseTime {
 
-	 @Test
-	    public void checkApiResponseTimeIsWithinRange() {
-	        // Get the current time in seconds before making the API call
-	        long currentTimestampBeforeApiCall = System.currentTimeMillis() / 1000; // current time in seconds
+	@Test
+    public void checkApiResponseTimeIsWithinRange() {
+        // Get the current time in seconds before making the API call
+        long currentTimestampBeforeApiCall = System.currentTimeMillis() / 1000; // current time in seconds
 
-	        // API Endpoint
-	        String baseURL = "https://open.er-api.com/v6/latest/USD"; // replace with your real API endpoint
+        // API Endpoint
+        String baseURL = "https://open.er-api.com/v6/latest/USD"; // replace with your real API endpoint
 
-	        // Make the API call and store the response
-	        Response response = given()
-	            .when()
-	                .get(baseURL);
+        // Make the API call and store the response
+        Response response = given().relaxedHTTPSValidation()
+            .when()
+                .get(baseURL);
 
-	        // Capture the API response timestamp from the body
-	        JsonPath jsonPathEvaluator = response.jsonPath();
-	        long apiResponseTimestamp = jsonPathEvaluator.getLong("timestamp");
+    // Check that the response time is not less than 3 seconds from the current time
+    ValidatableResponse respTime = response.then();
+    respTime.time(Matchers.greaterThan(3000L)); 	
 
-	        // Calculate the difference in seconds
-	        long differenceInSeconds = apiResponseTimestamp - currentTimestampBeforeApiCall;
-
-	        // Check that the response time is not less than 3 seconds from the current time
-	        Assert.assertTrue(differenceInSeconds >= 3, "The API response timestamp was less than 3 seconds from the sent request time.");
-	        
-	        // ... perform other response validations, if necessary.
-	    }
 	}
+}
